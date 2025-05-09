86f99c3189e0cf9caa86105ee50f2f400e1892ea
PengZai <649365461@qq.com>
2025-05-10 00:42:42 +0100

    add Dockerfile

---
+++ Docker/run_dev.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PROJECT_NAME="dsol"
+IMAGE_NAME="${PROJECT_NAME}:noetic-ros"
+DATA_PATH="/media/${USER}/zp_video/datasets"
+# Pick up config image key if specified
+if [[ ! -z "${CONFIG_DATA_PATH}" ]]; then
+    DATA_PATH=$CONFIG_DATA_PATH
+fi
+
+
+
+docker build -t $IMAGE_NAME -f "${HOME}/vscode_projects/${PROJECT_NAME}/catkin_ws/src/${PROJECT_NAME}/Docker/Dockerfile" .
+
+
+xhost +local:root
+
+docker run \
+    --rm \
+    -e DISPLAY \
+    -v ~/.Xauthority:/root/.Xauthority:rw \
+    --network host \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v ${HOME}/vscode_projects/${PROJECT_NAME}/catkin_ws:/root/catkin_ws \
+    -v ${DATA_PATH}:/root/datasets \
+    --privileged \
+    --cap-add sys_ptrace \
+    --runtime=nvidia \
+    --gpus all \
+    -it --name $PROJECT_NAME $IMAGE_NAME /bin/bash
+
+# docker run --rm -it --name $PROJECT_NAME $IMAGE_NAME /bin/bash
+
+xhost -local:root
